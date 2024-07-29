4b9013756a304c7082f8c10422fc39cc9555567b
OreoluwaA77 <oresadewunmi@gmail.com>
2024-07-29 23:39:44 +0100

    Update Festivaldiscounts.py

--- Conditional Statements/Festivaldiscounts.py
+++ Conditional Statements/Festivaldiscounts.py
@@ -4,3 +4,8 @@ if money_spent>1000:
     print(money_spent,"Amount to be paid after discount:",money_spent*0.5)
 elif 1000>=money_spent>=501:
     print(money_spent,"Amount to be paid after discount:",money_spent*0.35)
+elif 501>=money_spent>=201:
+    print(money_spent,"Amount to be paid after discount:",money_spent*0.25)
+elif money_spent<200:
+    print(money_spent,"Amount to be paid after discount:",money_spent*0.05)
+    
