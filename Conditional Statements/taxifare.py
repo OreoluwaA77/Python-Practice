a559ca023ae849c8853b75cc70a309d3b4aaeabe
OreoluwaA77 <oresadewunmi@gmail.com>
2024-07-31 11:41:54 +0100

    taxi fare updated

--- Conditional Statements/taxifare.py
+++ Conditional Statements/taxifare.py
@@ -1,7 +1,5 @@
-km_travelled=float(input("Please entrer the kilometres you covered")) 
-if km_travelled<=10: 
-  print("Amount payable is $", km_travelled*11) 
-elif 100>=km_travelled>10: 
-  print("Amount payable is $", (km_travelled-km_travelled+10)*11 +(km_travelled-10)*10) 
-elif km_travelled>100: 
-  print("Amount payable is $", (km_travelled-km_travelled+10)*11 +(km_travelled-km_travelled+90)*10 + (km_travelled-100)*9)
+km_travelled=float(input("Please entrer the kilometres you covered"))
+if km_travelled<=10:
+  print("Amount payable is $", km_travelled*11)
+elif 100>=km_travelled>10:
+  print("Amount payable is $", (km_travelled-km_travelled+10)*11 +(km_travelled-10)*10)
