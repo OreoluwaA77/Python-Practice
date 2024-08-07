8788ebdf6f7f57900f5dfd17ca9559b4a3cacb46
OreoluwaA77 <oresadewunmi@gmail.com>
2024-08-07 11:59:32 +0100

    sumof5 done

---
+++ Loops/sumof5.py
@@ -0,0 +1,7 @@
+number_1=int(input("Please enter your first interger value"))
+number_2=int(input("Please enter your second interger value"))
+number_3=int(input("Please enter your third interger value"))
+number_4=int(input("Please enter your fourth interger value"))
+number_5=int(input("Please enter your fifth interger value"))
+print("The sum of the five numbers is", number_1+number_2+number_3+number_4+number_5)
+
---
+++ Loops/sumof5_2.py
@@ -0,0 +1,5 @@
+total=0
+for i in range(0,5):
+    number=int(input("Enter a number"))
+    total=total+number
+print(total)
