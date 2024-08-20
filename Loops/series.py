1f11b5cdbc311a5d9c659b31fe0fb8aaa1398e13
OreoluwaA77 <oresadewunmi@gmail.com>
2024-08-20 22:31:50 +0100

    series improved

--- Loops/series.py
+++ Loops/series.py
@@ -6,7 +6,14 @@ for i in range(1,11):
 print(total)
 
 total=0
-for i in range(1,11):
+for i in range(1,10):
     total= total + (i*(i+1))
     #wrong?? confused
+print(total)
+
+total=0
+for i in range(0,11):
+    total=total + ((i+1)*(i+2))
+    #1*2+2*3 WRONG
+    #
 print(total)
