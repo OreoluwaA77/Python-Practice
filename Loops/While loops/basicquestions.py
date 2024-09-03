afc050520d30becc0daffb77af5b4f3abf5648af
OreoluwaA77 <oresadewunmi@gmail.com>
2024-09-03 11:13:17 +0100

    basicquestions

---
+++ Loops/While loops/basicquestions.py
@@ -0,0 +1,23 @@
+i=20
+while i<=50:
+    print (i)
+    i=i+2
+
+print("--------------")
+
+i=1
+while i<=100:
+    if i%3==0 and i%5==0:
+        print (i)  
+    i=i+1  
+
+print("--------------")
+
+i=1 
+number=int(input("Please enter a number")) 
+while i<=number: 
+    if number%i==0: 
+        print (i) 
+    i=i+1 
+
+print("--------------")
--- Loops/series.py
+++ Loops/series.py
@@ -1,19 +1,14 @@
-total=0
-for i in range(1,11):
-    total=total + (i*i)
-    #i*2 1+4+9+16...
-    #wrong???/
-print(total)
+total = 0 
+for i in range(1, 11): 
+    total=total+ (1/i) 
+print(total) 
 
 total=0
 for i in range(1,10):
     total= total + (i*(i+1))
-    #wrong?? confused
 print(total)
 
 total=0
-for i in range(0,11):
-    total=total + ((i+1)*(i+2))
-    #1*2+2*3 WRONG
-    #
+for i in range(1,11,2):
+     total= total + (i*(i+1))
 print(total)
