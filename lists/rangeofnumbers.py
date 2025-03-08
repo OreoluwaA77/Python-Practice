05b1b13954df809decb7e807c7d5513208581258
OreoluwaA77 <oresadewunmi@gmail.com>
2025-03-08 12:18:55 +0000

    upates

--- lists/rangeofnumbers.py
+++ lists/rangeofnumbers.py
@@ -7,3 +7,16 @@ def range_of_num (start,end):
 
 range_of_num(5, 9) #➞ [6, 7, 8] 
  
+
+def list_less_than(number):
+    total=0
+    for i in range (len(number)):
+        total=total+number[i]
+    print(total)
+    if total<100:
+        print(True)
+    else:
+        print(False)
+
+list_less_than([1,2,3,4,5,6,7])
+list_less_than([95,10,12])
