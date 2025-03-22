fe640873885e61e622c8957396d0ae0a70965d62
OreoluwaA77 <oresadewunmi@gmail.com>
2025-03-22 10:04:38 +0000

    updates

--- lists/practicelongtask.py
+++ lists/practicelongtask.py
@@ -49,3 +49,31 @@ random.remove("bottle")
 random.pop(-1)
 random.pop(2)
 print(random)
+
+#task 7
+scores=[98, 85, 76, 90, 100, 67, 80]
+total=0
+count=0
+largest_score=scores[0]
+smallest_score=scores[0]
+for i in range (len(scores)):
+    total=total+scores[i]
+    count=count+1
+
+    if largest_score<scores[i]:
+        largest_score=scores[i]
+    elif smallest_score>scores[i]:
+        smallest_score=scores[i]  
+average=total/(len(scores))
+print(largest_score)
+print(smallest_score)
+print(total)
+print(average)
+
+#task 8
+list_3=[1, 2, 2, 3, 4, 4, 5, 6, 6, 7]
+new_list_3=[]
+for i in range (len(list_3)):
+    if list_3[i] not in new_list_3:
+        new_list_3.append(list_3[i])
+print(new_list_3)
