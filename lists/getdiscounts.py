c4950d0c2bd83822f086c7c781440bbf1a028f0a
OreoluwaA77 <oresadewunmi@gmail.com>
2025-04-05 09:40:19 +0100

    updates

--- lists/getdiscounts.py
+++ lists/getdiscounts.py
@@ -1,6 +1,9 @@
 def get_discounts (numbers, discount):
+    multiplier=discount[0:-1:]
+    multiplier=int(multiplier)
     for i in range(len(numbers)):
-        applied_dicounts=numbers[i]*(discount/100)
+        applied_dicounts=numbers[i]*(multiplier/100)
         print(applied_dicounts)
 
-get_discounts([100,20,40],50)
+get_discounts([100,20,40],"50%")
+get_discounts([100,20,40],"1%")
---
+++ lists/looplist.py
@@ -0,0 +1,8 @@
+fruits=[]
+for i in range (0,5):
+    fruit=(input("Enter your favourite fruits"))
+    fruits.append(fruit)
+fruits_list="_".join(fruits)
+
+print(fruits)
+print(fruits_list)
