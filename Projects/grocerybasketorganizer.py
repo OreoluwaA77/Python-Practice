615da00a5f05a85fc72b26a0f49604438c9b4e20
OreoluwaA77 <oresadewunmi@gmail.com>
2025-04-22 15:08:39 +0100

    updates

--- Projects/grocerybasketorganizer.py
+++ Projects/grocerybasketorganizer.py
@@ -1,4 +1,18 @@
-
+new_grovery_basket_list=[]
+grocery_basket=(input("Enter your grocery list"))
+grocery_basket_list=grocery_basket.split(",")
+for i in range (len(grocery_basket_list)):
+    grocery_basket_list[i]=grocery_basket_list[i].replace(" ","")
+    if grocery_basket_list[i] not in new_grovery_basket_list:
+        new_grovery_basket_list.append(grocery_basket_list[i])
+new_grovery_basket_list.sort()
+print(new_grovery_basket_list)
+search=(input("What item are you looking for in your basket?"))
+if search in new_grovery_basket_list:
+    print ("Your item is in good hands")
+else:
+    print("Uh oh we can't find your item")
+    
 def remove_duplicates_and_sort (stringlist):
     stringlist_list=stringlist.split(",")
     final_list=[]
@@ -7,6 +21,7 @@ def remove_duplicates_and_sort (stringlist):
         if stringlist_list[i] not in final_list:
             final_list.append(stringlist_list[i])
     final_list.sort()
+    print(final_list)
     return(final_list)
 
 def search(final_list):
